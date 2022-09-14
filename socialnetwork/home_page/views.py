d6984bff5f634a0691c019f8ee5fc70c7a74fd18
Quentin Hors <hors.quentin@outlook.fr>
2022-09-14 16:08:25 +0200

    Ajout de la visibilité des post de l'utilisateur

--- socialnetwork/home_page/views.py
+++ socialnetwork/home_page/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from profile_page.models import Post
 
 
 @login_required
 def home_page(request):
-    return render(request, 'home_page/index.html')
+    user = request.user
+    user_posts = Post.objects.filter(user__id=user.id)
+    context = {'user_posts': user_posts}
+    return render(request, 'home_page/index.html', context=context)
