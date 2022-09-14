f6e136c38cf1258595370ad3b18b977a88b238ee
Quentin Hors <hors.quentin@outlook.fr>
2022-09-14 16:09:59 +0200

    Ajout des roots pour les vue de création/modification/suppréssion des post

--- socialnetwork/profile_page/urls.py
+++ socialnetwork/profile_page/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import profile_page, create_profile, update_profile
+from .views import profile_page, create_profile, update_profile, create_post, update_post, delete_post
 
 urlpatterns = [
     path('', profile_page, name='profile'),
     path('create', create_profile, name='create_profile'),
-    path('update', update_profile, name='update_profile')
+    path('update', update_profile, name='update_profile'),
+    path('post/create', create_post, name='create_post'),
+    path('post/update/<post_id>', update_post, name='update_post'),
+    path('post/delete/<post_id>', delete_post, name='delete_post')
 ]
