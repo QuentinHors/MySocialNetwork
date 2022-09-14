40eed6da98a240dff6c75820aefc82af27abdcb3
Quentin Hors <hors.quentin@outlook.fr>
2022-09-14 16:09:11 +0200

    Ajout des vues de création/modification/suppréssion des post

--- socialnetwork/profile_page/views.py
+++ socialnetwork/profile_page/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import ProfileForms
-from .models import Profile
+from .forms import ProfileForms, PostForm
+from .models import Profile, Post
 import os
 
 
 @login_required
 def profile_page(request):
-    if Profile.objects.filter(user__id=request.user.id).exists():
-        context = {'already_created': True}
+    user = request.user
+    user_posts = Post.objects.filter(user__id=user.id)
+    context = {'user_posts': user_posts}
+    if Profile.objects.filter(user__id=user.id).exists():
+        context['already_created'] = True
     else:
-        context = {'already_created': False}
+        context['already_created'] = False
     return render(request, 'profile_page/index.html', context=context)
 
 
@@ -56,3 +59,56 @@ def update_profile(request):
     form = ProfileForms(instance=profile)
     context = {'form': form, 'profile': profile}
     return render(request, 'profile_page/update_profile.html', context=context)
+
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
+            form.save()
+            redirection_page = request.POST.get('next')
+            return redirect(redirection_page)
+
+        context = {'form': form, 'errors': 'Informations invalides'}
+        return render(request, 'profile_page/create_profile.html', context=context)
+
+    form = PostForm()
+    context = {'form': form}
+    return render(request, 'profile_page/create_post.html', context=context)
+
+
+@login_required
+def update_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        image_post = post.image
+        if form.is_valid():
+            if 'image' in form.changed_data and image_post:
+                os.remove(image_post.path)
+            form.save()
+            redirection_page = request.POST.get('next')
+            return redirect(redirection_page)
+
+        context = {'form': form,
+                   'profile': post,
+                   'errors': 'Informations invalides'}
+        return render(request, 'profile_page/update_post.html', context=context)
+
+    form = PostForm(instance=post)
+    context = {'form': form}
+    return render(request, 'profile_page/update_post.html', context=context)
+
+
+@login_required
+def delete_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    image_post = post.image
+    if image_post:
+        os.remove(path=image_post.path)
+    post.delete()
+    redirection_page = request.GET.get('next')
+    return redirect(redirection_page)
