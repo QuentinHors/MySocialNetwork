ac40397dff4c463032e7e30982b601b1ca5c056c
Quentin Hors <hors.quentin@outlook.fr>
2022-09-28 13:56:07 +0200

    Ajout des fonctionnalités des commentaire

--- socialnetwork/home_page/views.py
+++ socialnetwork/home_page/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from profile_page.models import Post
+from profile_page.models import Post, Comment
 
 
 @login_required
 def home_page(request):
     user = request.user
     user_posts = Post.objects.filter(user__id=user.id)
-    context = {'user_posts': user_posts}
+    user_posts_and_comments = []
+    for post in user_posts:
+        user_posts_and_comments.append((post, Comment.objects.filter(post_reference__id=post.id)))
+    context = {'user_posts': user_posts_and_comments}
     return render(request, 'home_page/index.html', context=context)
