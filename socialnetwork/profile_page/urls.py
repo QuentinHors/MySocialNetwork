f98ba12b1fc7bd176e45cd85981169ba05f35b54
Quentin Hors <hors.quentin@outlook.fr>
2022-09-28 13:58:11 +0200

    Ajout des roots pour les commentaires

--- socialnetwork/profile_page/urls.py
+++ socialnetwork/profile_page/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import profile_page, create_profile, update_profile, create_post, update_post, delete_post
+from .views import profile_page, create_profile, update_profile, create_post, update_post, delete_post, create_comment, update_comment, delete_comment
 
 urlpatterns = [
     path('', profile_page, name='profile'),
@@ -7,5 +7,8 @@ urlpatterns = [
     path('update', update_profile, name='update_profile'),
     path('post/create', create_post, name='create_post'),
     path('post/update/<post_id>', update_post, name='update_post'),
-    path('post/delete/<post_id>', delete_post, name='delete_post')
+    path('post/delete/<post_id>', delete_post, name='delete_post'),
+    path('comment/create/<post_id>', create_comment, name='create_comment'),
+    path('comment/update/<comment_id>', update_comment, name='update_comment'),
+    path('comment/delete/<comment_id>', delete_comment, name='delete_comment')
 ]
